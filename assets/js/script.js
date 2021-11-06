4687d06b926a58b085de7ee32fe5fa18690af146
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-05 21:22:17 -0500

    Declaring variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,23 @@ var questions = [
         
     },
 ];
+
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+
+// Start working code 
+// Declared variables
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+// Seconds left is 15 seconds per question:
+var secondsLeft = 76;
+// Holds interval time
+var holdInterval = 0;
+// Holds penalty time
+var penalty = 10;
+// Creates new element
+var ulCreate = document.createElement("ul");
